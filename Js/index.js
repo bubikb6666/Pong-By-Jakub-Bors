a53465c63790b0cd0611ce4a1b3bad7162cc5d1d
bubikb6666 <108403721+bubikb6666@users.noreply.github.com>
2022-09-20 17:03:24 +0200

    Update

--- Js/index.js
+++ Js/index.js
@@ -1,9 +1,6 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = 1000;
-canvas.height = 500;
-
 const cw = canvas.width;
 const ch = canvas.height;
 const ballSize = 20;
